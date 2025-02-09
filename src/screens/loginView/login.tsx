7abe58e89b68e963943b3b56c03e8822ab59d884
TheWoxPL <wojtek.bubula@onet.pl>
2025-02-09 23:11:06 +0100

    feat: another test requests

--- src/screens/loginView/login.tsx
+++ src/screens/loginView/login.tsx
@@ -32,12 +32,15 @@ const Login: React.FC = () => {
       return;
     }
     try {
-      const response = await fetch('http://localhost:4455/api/v1/user-route', {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${token}`,
+      const response = await fetch(
+        'http://localhost:4455/api/v1/student-route',
+        {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         },
-      });
+      );
       const data = await response.text();
       console.log('User Route Response:', data);
       alert(data);
@@ -47,12 +50,90 @@ const Login: React.FC = () => {
     }
   };
 
+  const handlePresidentRoute = async () => {
+    const token = await getToken();
+    if (token === null) {
+      alert('Please login first');
+      return;
+    }
+    try {
+      const response = await fetch(
+        'http://localhost:4455/api/v1/president-route',
+        {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+      const data = await response.text();
+      console.log('President Route Response:', data);
+      alert(data);
+    } catch (error) {
+      console.error('President route failed:', error);
+      alert('President route failed');
+    }
+  };
+
+  const addStudentRole = async () => {
+    const token = await getToken();
+    if (token === null) {
+      alert('Please login first');
+      return;
+    }
+    try {
+      const response = await fetch(
+        'http://localhost:4455/api/v1/accounts/add-student-role',
+        {
+          method: 'POST',
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+      const data = await response.text();
+      console.log('Student Route Response:', data);
+      alert(data);
+    } catch (error) {
+      console.error('Student route failed:', error);
+      alert('Student route failed');
+    }
+  };
+
+  const addPresidentRole = async () => {
+    const token = await getToken();
+    if (token === null) {
+      alert('Please login first');
+      return;
+    }
+    try {
+      const response = await fetch(
+        'http://localhost:4455/api/v1/accounts/add-president-role',
+        {
+          method: 'POST',
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+      const data = await response.text();
+      console.log('Response:', data);
+      alert(data);
+    } catch (error) {
+      console.error('Route failed:', error);
+      alert('President route failed');
+    }
+  };
+
   return (
     <div>
       <button onClick={googleLogin}>Login with Google</button>
-      <button onClick={handleAdminRoute}>Test Admin Route</button>
-      <button onClick={handleUserRoute}>Test User Route</button>
+      <button onClick={handleAdminRoute}>Test ADMIN Route</button>
+      <button onClick={handleUserRoute}>Test STUDENT Route</button>
+      <button onClick={handlePresidentRoute}>Test PRESIDENT Route</button>
       <button onClick={logout}>Logout</button>
+      <button onClick={addStudentRole}>Add student role</button>
+      <button onClick={addPresidentRole}>Add president role</button>
     </div>
   );
 };
