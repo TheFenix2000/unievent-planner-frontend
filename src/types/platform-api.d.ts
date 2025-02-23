8fb4b3a5ef4387cf8bc0aea670c02ff6b8f1ace7
grochalapiotr <miami2042@gmail.com>
2025-02-23 20:08:58 +0100

    feat: add more dtos to platform-api

--- src/types/platform-api.d.ts
+++ src/types/platform-api.d.ts
@@ -16,7 +16,7 @@ export enum AnnouncementTargetType {
 export enum EventTypeColor {
   CLASSES = '#BBE2FF',
   CLASS_CANCELLATION = '#FFF069',
-  TESTS = '#F1656A',
+  TESTS = '#F1656A', // EXAM, RETAKES, QUIZ
   MEETING = '#B8FFA8',
   OTHER = '#D9D9D9',
 }
@@ -85,4 +85,93 @@ export interface AccountBasicDto extends BasicDto {
   avatarUrl?: string;
   fullName: string;
   initials: string;
+}
+
+export interface UserAccountDto extends AccountBasicDto {
+  firstName?: string;
+  lastName?: string;
+  email: string;
+  roles: RoleDto[];
+  groups: GroupDto[];
+  status: SystemStatus;
+}
+
+export interface AnnouncementDto extends BaseDto {
+  title: string;
+  description: string;
+  expiresAt?: Date;
+  important: boolean;
+}
+
+export interface EventDto extends BaseDto {
+  title: string;
+  description: string;
+  startDate: Date;
+  endDate: Date;
+  groups: string[];
+  type: EventType;
+}
+
+export interface EventTypeDto {
+  color: EventTypeColor;
+  type: EventType;
+}
+
+export interface CreateGroupDto {
+  name: string;
+  courseName?: string;
+  description?: string;
+  avatarUrl: string;
+}
+
+export interface GroupDto extends BaseDto {
+  name: string;
+  courseName?: string;
+  description?: string;
+  avatarUrl: string;
+  code: string;
+  status: SystemStatus;
+  verificationStatus: VerificationStatus;
+  rejectionReason?: string;
+  president: AccountBasicDto;
+  members: AccountBasicDto[];
+  joinCodes: JoinCodeDto[];
+}
+
+export interface JoinCodeDto extends BaseDto {
+  name: string;
+  code: string;
+  role: RoleDto;
+  group: GroupDto;
+  status: SystemStatus;
+  uses: number;
+  usesLeft?: number;
+  expiresAt: Date;
+}
+
+export interface PermissionRule {
+  action: Action;
+  subject: Subject;
+}
+
+export interface RoleDto extends BaseDto {
+  name: string;
+  code: string;
+  permissions: PermissionRule[];
+  status: SystemStatus;
+}
+
+export interface CreateSystemLogDto {
+  action: SystemLogAction;
+  message: string;
+  context: SystemLogContext;
+  relatedObjectId: string;
+}
+
+export interface SystemLogDto extends BaseDto {
+  action: SystemLogAction;
+  message: string;
+  context: SystemLogContext;
+  relatedObjectId: string;
+  status: SystemStatus;
 }
